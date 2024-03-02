3d44e082a26b24aca90788eeed873d60b5b3a4c5
Josmarxd <josmarhuchim@outlook.com>
2024-03-02 15:26:25 -0600

    commit

--- CarHup/CarHup.java
+++ CarHup/CarHup.java
@@ -3,7 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class CarHup extends JFrame implements ActionListener{
+    JDialog ventanaLogin;
+    JDialog ventana2;
     JPanel norte;
+    ImageIcon logoVna;
 
     //PANEL NORTE
     JLabel nombreU;
@@ -22,10 +25,10 @@ public class CarHup extends JFrame implements ActionListener{
       
 
     public CarHup(){
-        norte = new JPanel();
-        
-        
+        ventanaLogin= new JDialog();
+        ventana2 = new JDialog(this, "VENTANA 2",true);
 
+        norte = new JPanel();
         nombreU = new JLabel("Hola"+ " Josmar");
         nombreCarHup = new JLabel("CARHUP");
         logo = new JLabel();
@@ -35,7 +38,7 @@ public class CarHup extends JFrame implements ActionListener{
         configuracionButton = new JButton("Configuración");
         modoOscuroButton = new JButton("Modo Oscuro");
         miInformacionButton = new JButton("Mi Información");
-        
+        logoVna = new ImageIcon("CarHup/Imagenes/Loficial.png");
         init();
         
     }
@@ -43,12 +46,23 @@ public class CarHup extends JFrame implements ActionListener{
 
     private void init(){
        this.setVisible(true);
-       this.setSize(400,400);
+       this.setSize(900,900);
        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
        this.setLayout(new BorderLayout());
        this.getContentPane().setBackground(new Color(255, 218, 185));
+       this.setLocationRelativeTo(null);
+       this.setTitle("CARHUP");
+       this.setIconImage(logoVna.getImage());
 
        norte();
+
+       // Registra ActionListener para los botones
+    inicioButton.addActionListener(this);
+    buscarButton.addActionListener(this);
+    loginButton.addActionListener(this);
+    configuracionButton.addActionListener(this);
+    modoOscuroButton.addActionListener(this);
+    miInformacionButton.addActionListener(this);
     }
     private void norte() {
         norte.setLayout(null);
@@ -62,27 +76,29 @@ public class CarHup extends JFrame implements ActionListener{
         loginButton.setBounds(540, 150, 70, 30);
         buscarButton.setBounds(630, 150, 80, 30);
         configuracionButton.setBounds(720, 150, 120, 30);
-
-
         // Agregar "CARHUP" al panel
         norte.add(nombreCarHup);
         norte.add(inicioButton);
         norte.add(loginButton);
         norte.add(buscarButton);
         norte.add(configuracionButton);
-
         // Configuración del logo
         ImageIcon logoimg = new ImageIcon("CarHup/Imagenes/Loficial.png");
         logo.setIcon(new ImageIcon(logoimg.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
         logo.setBounds(600, 0, 100, 250);
-
         // Agregar el logo al panel
         norte.add(logo);
-
+        
         this.getContentPane().add(norte, BorderLayout.NORTH);
     }
+
     private void login() {
-        // Implementar funcionalidad de login
+        ventana2.setVisible(true);
+        ventana2.setSize(400,550);
+        ventana2.setLocationRelativeTo(null);
+        ventana2.setTitle("Iniciar secion o Crear Cuenta");
+        ventana2.setIconImage(logoVna.getImage());
+        add(ventana2);
     }
 
     private void buscarConductor() {
@@ -90,7 +106,15 @@ public class CarHup extends JFrame implements ActionListener{
     }
 
     private void configuracion() {
-        
+
+    }
+
+    private void modoOscuro(){
+
+    }
+
+    private void miInformacion(){
+
     }
 
     private void inicio(){
@@ -108,10 +132,9 @@ public class CarHup extends JFrame implements ActionListener{
         } else if (e.getSource() == loginButton) {
             login();
         } else if (e.getSource() == modoOscuroButton) {
-            
+            modoOscuro();
         } else if (e.getSource() == miInformacionButton) {
-            
-            
+           miInformacion();
         }
     }
     public static void main(String[] args) {
